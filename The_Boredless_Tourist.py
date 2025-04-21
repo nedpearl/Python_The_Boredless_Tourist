870ef5f40c4a2c50ebba3c038998062f4ef0ce96
nedpearl <161415572+nedpearl@users.noreply.github.com>
2025-04-21 12:11:42 +0800

    Added test objects

--- The_Boredless_Tourist.py
+++ The_Boredless_Tourist.py
@@ -1,1 +1,2 @@
-print("Hello")
+destination = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'São Paulo, Brazil', 'Cairo, Egypt']
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]

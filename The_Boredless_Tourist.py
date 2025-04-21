051890654b8f7651ff34bfda8d9f88932167e737
nedpearl <161415572+nedpearl@users.noreply.github.com>
2025-04-21 14:39:09 +0800

    Created attractions and functionality for adding new attractions

--- The_Boredless_Tourist.py
+++ The_Boredless_Tourist.py
@@ -9,6 +9,7 @@ def get_destination_index(destination):
 
 #print(get_destination_index("Hyderabad, India"))
 
+#getting the traveler's destination's index from the destinations list
 def get_traveler_location(traveler):
     traveler_destination = traveler[1]
     traveler_destination_index = get_destination_index(traveler_destination)
@@ -16,4 +17,28 @@ def get_traveler_location(traveler):
     return traveler_destination_index
 
 test_destination_index = get_traveler_location(test_traveler)
-print(test_destination_index)
+#print(test_destination_index)
+
+attractions = [[] for d in destinations]
+#print(attractions)
+
+#adding attractions to attractions list
+def add_attraction(destination, attraction):
+    destination_index = get_destination_index(destination)
+    attractions_for_destination = attractions[destination_index]
+    attractions_for_destination.append(attraction)
+    return attractions_for_destination
+
+add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
+add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
+add_attraction("Shanghai, China", ["Yu Garden", ["garden", "historical site"]])
+add_attraction("Shanghai, China", ["Yuz Museum", ["art", "museum"]])
+add_attraction("Shanghai, China", ["Oriental Pearl Tower", ["skyscraper", "viewing deck"]])
+add_attraction("Los Angeles, USA", ["LACMA", ["art", "museum"]])
+add_attraction("São Paulo, Brazil", ["São Paulo Zoo", ["zoo"]])
+add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]])
+add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
+add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
+
+#print(add_attraction('Los Angeles, USA', ['Venice Beach', ['beach']]))
+print(attractions)
